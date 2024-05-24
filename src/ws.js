e54588b04edb8889a1640d17696102bb73385679
Radu Crisan <radu.crisan@ekkode.com>
2024-05-24 10:22:07 +0300

    add ws

---
+++ src/ws.js
@@ -0,0 +1,14 @@
+import {useTasksStore} from "./stores/tasks.js"
+
+export const ws = new WebSocket('ws://localhost:8080')
+
+ws.onopen = () => {
+ console.log(`Connected to server`)
+}
+
+ws.onmessage = (ws) => {
+//  const taskStore = useTasksStore();
+//  taskStore.lists = JSON.parse(ws.data);
+
+ console.log('Message from server:', ws.data)
+}
